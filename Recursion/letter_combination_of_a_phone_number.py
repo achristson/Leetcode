8a639c072b2aa43e2f6cf3bca7e938e76b858f0e
achristson <christsona0@outlook.com>
2022-04-30 13:51:40 -0500

    added in place capacity solution

--- Recursion/letter_combination_of_a_phone_number.py
+++ Recursion/letter_combination_of_a_phone_number.py
@@ -31,3 +31,37 @@ Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 Time: O(4^n)
 Space: O(4^n)
 """
+
+
+def letter_combinations_capacity(digits):
+    if not digits:
+        return []
+
+    mappings = {"2": "abc",
+                "3": "def",
+                "4": "ghi",
+                "5": "jkl",
+                "6": "mno",
+                "7": "pqrs",
+                "8": "tuv",
+                "9": "wxyz"}
+    result = []
+    slate = [""] * len(digits)
+
+    def helper(i):
+        if i == len(digits):
+            result.append("".join(slate))
+            return
+
+        for j in range(len(mappings[digits[i]])):
+            slate[i] = mappings[digits[i]][j]
+            helper(i+1)
+
+    helper(0)
+    return result
+
+
+"""
+Time: O(4^n)
+Space: O(4^n)
+"""
