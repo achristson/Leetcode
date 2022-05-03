70cbc417760af8df64bd77c4bdf9ad9e8e5c0945
achristson <christsona0@outlook.com>
2022-05-02 20:12:41 -0500

    added basic fibonacci solution with complexities

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -7,16 +7,31 @@ And you start with 1 and 1
 
 Write a program that given n gives you the n'th fib number.
 """
+
+
+def fibonacci(n):
+    """Memoization Solution"""
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+"""
+Time: O(2^n)
+Space: O(n)
+"""
+
+
 cache = {}
 cache[1] = 1
 cache[2] = 1
 
 
-def fibonacci(n):
+def fibonacci_memo(n):
     """Memoization Solution"""
     if n in cache:
         return cache[n]
-    cache[n] = fibonacci(n-1) + fibonacci(n-2)
+    cache[n] = fibonacci_memo(n - 1) + fibonacci_memo(n - 2)
     return cache[n]
 
 
@@ -31,12 +46,12 @@ def fibonacci_dp(n):
     if n == 1 or n == 2:
         return 1
 
-    table = [0 for _ in range(n+1)]
+    table = [0 for _ in range(n + 1)]
     table[1] = 1
     table[2] = 1
 
-    for i in range(3, n+1):
-        table[i] = table[i-1] + table[i-2]
+    for i in range(3, n + 1):
+        table[i] = table[i - 1] + table[i - 2]
     return table[n]
 
 
@@ -56,7 +71,7 @@ def fibonacci_dp_constant_space(n):
     table[2] = 1
 
     for i in range(3, n+1):
-        table[i % 3] = table[(i-1) % 3] + table[(i-2) % 3]
+        table[i % 3] = table[(i - 1) % 3] + table[(i - 2) % 3]
     return table[n % 3]
 
 
